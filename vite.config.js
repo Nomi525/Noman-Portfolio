0964f7ddc6dea3fd1fe806bb80d6e302105f1185
NomanMansuri <nomanmansuri525@gmail.com>
2024-10-14 17:21:36 +0530

    init for deploy the code

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  // base: '/Noman-Portfolio/', // Replace with your repository name
+  base: '/Noman-Portfolio/', // Replace with your repository name
 });
